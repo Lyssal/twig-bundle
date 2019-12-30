04be467e4770757c123b5678824b6bccdf6bfe2c
Rémi Leclerc <remi.leclerc@lyssal.com>
2019-12-31 00:15:06 +0100

    Services : Use YAML

--- src/DependencyInjection/LyssalTwigExtension.php
+++ src/DependencyInjection/LyssalTwigExtension.php
@@ -25,7 +25,7 @@ class LyssalTwigExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
     }
 }
